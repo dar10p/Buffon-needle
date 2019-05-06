5519ac6944ead1dac138c6f112e7f9a2cb583160
Dario Perez <49408138+dar10p@users.noreply.github.com>
2019-05-06 14:45:04 -0400

    Update Ejercicio_aguja.m
    Agregué comentarios. Parece existir una compensación entre dos errores que finalmente aparece el resultado correcto. Ver 'Figura2.pdf'

--- Ejercicio_aguja.m
+++ Ejercicio_aguja.m
@@ -7,15 +7,20 @@
 % que una aguja de largo "d" (igual que el espaciado de lineas)corte una de
 % las lineas, de esta manera se pude encontrar el valor de pi.
 
- 
+%
+%% ESTE CODIGO SE DEBE ESCRIBIR COMO FUNCION.
+% function pi = Ejercicio_aguja(N,L,D) %por ejemplo.
+
 N = 150006000;   %n�mero de lanzamientos.
 L = 1;      %Longitud de la aguja.
 D = 1;      %Separaci�n entre las lineas horizontales.
 
+
+
 Nc = 0;     %contador para saber cuantas veces la aguja cort� una de las 
             %lineas.
 
-%tomando en cuenta que para este ejemplo las lineas son horizontales
+%tomando en cuenta que para este ejemplo las lineas son horizontales <~VERTICALES???
 %separadas la misma distancia D entre ellas.
 
 for a = 1:N
@@ -24,21 +29,21 @@ for a = 1:N
 %la aguja y ademas el �ngulo con respecto a la horizontal con que cae 
 %la aguja.
 
-    X = (D/2)*rand; % X denota la distancia del centro de la aguja con 
-                    % alguna de las lineas del plano.
+    X = (D/2)*rand; % X denota la distancia del centro de la aguja con <~ SOLO MUESTRA DISTANCIA AL ORIGEN Y LA PRIMERA LINEA A LA DERECHA
+                    % alguna de las lineas del plano. <~ NO ES DE ALGUNA ES A DOS: UNA EN EL ORIGEN Y LA SIGUIENTE A LA DERECHA
                     
     T = 2*pi*rand;  % Es el �ngulo con el que cae la aguja con respecto a la
-                    % horizontal.
+                    % horizontal. <~ DA UNA VUELTA ENTERA, LA MITAD ES INNECESARIA, YENDO DE 0 A PI ALCANZA.
  
     if X <= (0.5)*L*sin(T) %Si esta condici�n se cumle entonces la aguja 
-                           %intersecta una de las lineas.
+                           %intersecta una de las lineas. <~HAY QUE REVISAR NO PARECE QUE VIERA TODOS LOS CASOS.
                         
         Nc = Nc + 1;
     else
         
     end
     
-    P  = (2*L)/(pi*D);  %Prpbabilidad que la aguja cort� una de las lineas.
+    P  = (2*L)/(pi*D);  %Prpbabilidad que la aguja cort� una de las lineas. <~ CALCULA NADA AQUI ELIMINAR.
     Pi = (N)/Nc;        %Estimacion del n�mero pi.
         
 end              
